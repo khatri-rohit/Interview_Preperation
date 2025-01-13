8d2292f57be4a37a099cc82b5f1f1cfc7e933c31
Rohit Khatri <111869337+khatri-rohit@users.noreply.github.com>
2025-01-13 20:04:49 +0000

    Java Interfaces

--- Javas/Welcome.java
+++ Javas/Welcome.java
@@ -1,6 +1,18 @@
 // package Javas;
+
+abstract class Trying {
+    String setName(String name) {
+        return name;
+    }
+}
+
+class Names extends Trying {
+
+}
+
 class Welcome {
     public static void main(String args[]) {
-        System.out.printf("%d\n", 1);
+        Names obj = new Names();
+        System.out.println(obj.setName("Rohit"));
     }
 }
