f7ab0d9ec24d7a00b7c61b5d62de0af640f0817e
Rohit Khatri <111869337+khatri-rohit@users.noreply.github.com>
2025-01-15 13:43:25 +0000

    Commit

--- Javas/Welcome.java
+++ Javas/Welcome.java
@@ -1,5 +1,7 @@
 // package Javas;
 
+import java.lang.System;
+
 abstract class Trying {
     String setName(String name) {
         return name;
@@ -7,12 +9,14 @@ abstract class Trying {
 }
 
 class Names extends Trying {
-
+    Names() {
+        System.out.println("Java Names Constructor");
+    }
 }
 
 class Welcome {
     public static void main(String args[]) {
         Names obj = new Names();
-        System.out.println(obj.setName("Rohit"));
+        System.out.println(obj.setName("Khatri"));
     }
 }
