6f523729d4e4571feda4f216ce21c3ed5209074e
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-02-02 00:17:07 +0530

    Recursion Question

--- DSA/Recursion/ReverseString.java
+++ DSA/Recursion/ReverseString.java
@@ -1,6 +1,8 @@
 // package DSA.Recursion;
 
 public class ReverseString {
+    static int count = 0;
+
     public static void reverse(String str, int i) {
         if (i < 0)
             return;
@@ -52,6 +54,52 @@ public class ReverseString {
         }
     }
 
+    public static String moveX(String str, String newString, int index) {
+        if (index == str.length()) {
+            while (count > 0) {
+                newString = newString + "x";
+                count--;
+            }
+            return newString;
+        }
+        if (str.charAt(index) != 'x') {
+            newString += str.charAt(index);
+        } else {
+            count++;
+        }
+        return moveX(str, newString, index + 1);
+    }
+
+    public static String removeDuplicate(String str, String newString, int index, boolean map[]) {
+        if (index == str.length()) {
+            return newString;
+        }
+        int idx = str.charAt(index) - 'a';
+        if (!map[idx]) {
+            newString += str.charAt(index);
+            map[idx] = true;
+        }
+        return removeDuplicate(str, newString, index + 1, map);
+        // if (!newString.contains(String.valueOf(str.charAt(index)))) {
+        // newString += str.charAt(index);
+        // }
+    }
+
+    static String[] keypad = { ".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    public static void printComb(String str, int index, String newString) {
+        if (index == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char curChar = str.charAt(index);
+        String mapping = keypad[curChar - '0'];
+        for (int i = 0; i < mapping.length(); i++) {
+            printComb(str, index + 1, newString + mapping.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
 
         // String rev = "abcd"; // Reverse String
@@ -60,7 +108,20 @@ public class ReverseString {
         // String str = "padhanbs"; // Reverse String
         // track(str, 'a', 0, -1, -1);
 
-        int arr[] = { 1, 2, 3, 4, 5 };
-        System.out.println(assending(arr, 0));
+        // int arr[] = { 1, 2, 3, 4, 5 };
+        // System.out.println(assending(arr, 0));
+
+        // String str = "axbcxxd";
+        // String newString = "";
+        // System.out.println(moveX(str, newString, 0));
+
+        // String str = "roihrit";
+        // String newString = "";
+        // boolean map[] = new boolean[26];
+        // System.out.println(removeDuplicate(str, newString, 0, map));
+
+        String str = "23";
+        printComb(str, 0, "");
+
     }
 }
